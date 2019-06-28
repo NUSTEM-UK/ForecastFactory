f771ac79a2653dfddcb5b40d2cc6726cfd6349b0
IvanRossCousins <ivan.cousins@northumbria.ac.uk>
2019-06-28 10:52:38 +0100

    Fixed addition issue

--- sketch.js
+++ sketch.js
@@ -1,43 +1,7 @@
-// var gradientColours = [[146,0,230], [226,223,25]]; 
-
 var colourGradient = palette('tol-rainbow', 500);
 console.log(colourGradient);
 
-// function pickHex(color1, color2, weight) {
-// 	console.log(color1);
-// 	console.log(color2);
-//     var w1 = weight;
-//     var w2 = 1 - w1;
-//     console.log(w1);
-//     console.log(w2);
-//     var rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
-//         Math.round(color1[1] * w1 + color2[1] * w2),
-//         Math.round(color1[2] * w1 + color2[2] * w2)];
-       
-//     return rgb;
-// }
-
-function hexToRGB(h) {
-	let r = 0, g = 0, b = 0;
-
-	// 3 digits
-	if (h.length == 4) {
-		r = "0x" + h[1] + h[1];
-		g = "0x" + h[2] + h[2];
-		b = "0x" + h[3] + h[3];
-
-		// 6 digits
-	} else if (h.length == 7) {
-		r = "0x" + h[1] + h[2];
-		g = "0x" + h[3] + h[4];
-		b = "0x" + h[5] + h[6];
-	}
-
-	return "rgb(" + +r + "," + +g + "," + +b + ")";
-}
-
 function convertHex(hex, opacity) {
-	// hex = hex.replace('#', '');
 	r = parseInt(hex.substr(0, 2), 16);
 	g = parseInt(hex.substr(2, 2), 16);
 	b = parseInt(hex.substr(4, 2), 16);
@@ -51,21 +15,36 @@ function convertHex(hex, opacity) {
 
 function tempToColour(temp) {
 	console.log(temp);
-	offsetTemp = temp + 10.0;
-	console.log(offsetTemp);
+	if (typeof temp === 'undefined' || !temp || temp == "-") {
+		temp = 0;
+	}
+	temp = parseFloat(temp);
+	console.log("t: " + temp);
+
+	offsetTemp = temp + 10;
+	console.log("ot: " + offsetTemp);
+
 	roundedTemp = round(offsetTemp, 1);
-	console.log(roundedTemp);
-	// colour = hexToRGB(colourGradient[roundedTemp * 10]);
-	colourIndex = parseInt(roundedTemp);
-	console.log(colourIndex);
-	hexColour = colourGradient[colourIndex];
+	console.log("rt: " + roundedTemp);
+
+	finalTemp = roundedTemp*10;
+	if (finalTemp >= 500)
+	{
+		finalTemp = 499;
+	}
+	else if (finalTemp < 0)
+	{
+		finalTemp = 0;
+	}
+	console.log("ft: " + finalTemp);
+
+	
+	hexColour = colourGradient[finalTemp];
 	console.log(hexColour);
+
 	colour = convertHex(hexColour, 100);
-	// colour = colourGradient[roundedTemp * 10];
+
 	return colour;
-	// normalisedTemp = offsetTemp/50;
-	// console.log(normalisedTemp);
-	// var colour = pickHex(gradientColours[0], gradientColours[1], normalisedTemp;
 }
 
 function setup() {
@@ -82,117 +61,117 @@ function draw() {
 		for (var y = 0; y < height; y += height/4) {
 			switch((x/25) + (y/100)) {
 			  case 0:
-					var temp = parseFloat(($("input[name='A1']").val()));
+				var temp = $("input[name='A1']").val();
 			    console.log("temp:" + temp);
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 1:
 			    var temp = $("input[name='B1']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 2:
 			    var temp = $("input[name='C1']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 3:
 			    var temp = $("input[name='D1']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 4:
 			    var temp = $("input[name='A2']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 5:
 			    var temp = $("input[name='B2']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 6:
 			    var temp = $("input[name='C2']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 7:
 			    var temp = $("input[name='D2']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 8:
 			    var temp = $("input[name='A3']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 9:
 			    var temp = $("input[name='B3']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
 			    // fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				fill(tempToColour(temp));
 			    break;
 			  case 10:
 			    var temp = $("input[name='C3']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 11:
 			    var temp = $("input[name='D3']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 12:
 			    var temp = $("input[name='A4']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 13:
 			    var temp = $("input[name='B4']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 14:
 			    var temp = $("input[name='C4']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  case 15:
 			    var temp = $("input[name='D4']").val();
 			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
 			    // console.log(colour);
-					// fill(colour[0], colour[1], colour[2]);
-					fill(tempToColour(temp));
+				// fill(colour[0], colour[1], colour[2]);
+				fill(tempToColour(temp));
 			    break;
 			  default:
 			    console.log("didn't work");
