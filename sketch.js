7bfad9a96cbbffcd7985765bea26a3e6cb3dc4d7
IvanRossCousins <ivan.cousins@northumbria.ac.uk>
2019-06-28 15:43:23 +0100

    First Finished Draft

--- sketch.js
+++ sketch.js
@@ -48,8 +48,7 @@ function tempToColour(temp) {
 }
 
 function setup() {
-	// bg = loadImage('');
-	createCanvas(400, 400);
+	createCanvas(800, 800);
 	background(200);
 	rectMode(CORNER);
 }
@@ -59,125 +58,77 @@ function draw() {
 	noFill();
 	for (var x = 0; x < width; x += width/4) {
 		for (var y = 0; y < height; y += height/4) {
-			switch((x/25) + (y/100)) {
+			switch((x/50) + (y/200)) {
 			  case 0:
 				var temp = $("input[name='A1']").val();
 			    console.log("temp:" + temp);
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 1:
 			    var temp = $("input[name='B1']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 2:
 			    var temp = $("input[name='C1']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 3:
 			    var temp = $("input[name='D1']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 4:
 			    var temp = $("input[name='A2']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 5:
 			    var temp = $("input[name='B2']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 6:
 			    var temp = $("input[name='C2']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 7:
 			    var temp = $("input[name='D2']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 8:
 			    var temp = $("input[name='A3']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 9:
 			    var temp = $("input[name='B3']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-			    // fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 10:
 			    var temp = $("input[name='C3']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 11:
 			    var temp = $("input[name='D3']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 12:
 			    var temp = $("input[name='A4']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 13:
 			    var temp = $("input[name='B4']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 14:
 			    var temp = $("input[name='C4']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  case 15:
 			    var temp = $("input[name='D4']").val();
-			    // var colour = pickHex(gradientColours[0], gradientColours[1], ((temp+10)/50));
-			    // console.log(colour);
-				// fill(colour[0], colour[1], colour[2]);
 				fill(tempToColour(temp));
 			    break;
 			  default:
 			    console.log("didn't work");
 			    noFill();
 			}
-			rect(x, y, 100, 100);
+			rect(x, y, width/4, height/4);
 		}
 	}
 }
---
+++ switch.js
@@ -0,0 +1,189 @@
+function setToInitialComp() {
+	$("input[name='A1']").val("5");
+	$("input[name='A2']").val("10");
+	$("input[name='A3']").val("20");
+	$("input[name='A4']").val("25");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("10");
+	$("input[name='B3']").val("20");
+	$("input[name='B4']").val("25");
+	$("input[name='C1']").val("5");
+	$("input[name='C2']").val("10");
+	$("input[name='C3']").val("20");
+	$("input[name='C4']").val("25");
+	$("input[name='D1']").val("5");
+	$("input[name='D2']").val("10");
+	$("input[name='D3']").val("20");
+	$("input[name='D4']").val("25");
+}
+
+function setTo1HourComp() {
+	$("input[name='A1']").val("5");
+	$("input[name='A2']").val("9.37");
+	$("input[name='A3']").val("18.452");
+	$("input[name='A4']").val("24.1");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("8.74");
+	$("input[name='B3']").val("16.904");
+	$("input[name='B4']").val("23.2");
+	$("input[name='C1']").val("5");
+	$("input[name='C2']").val("8.11");
+	$("input[name='C3']").val("15.32");
+	$("input[name='C4']").val("22.3");
+	$("input[name='D1']").val("4.55");
+	$("input[name='D2']").val("7.48");
+	$("input[name='D3']").val("13.772");
+	$("input[name='D4']").val("21.4");
+}
+
+function setTo2HoursComp() {
+	$("input[name='A1']").val("5");
+	$("input[name='A2']").val("8.6833");
+	$("input[name='A3']").val("16.9068");
+	$("input[name='A4']").val("23.0672");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("7.9563");
+	$("input[name='B3']").val("14.6551");
+	$("input[name='B4']").val("20.9496");
+	$("input[name='C1']").val("4.8607");
+	$("input[name='C2']").val("7.1726");
+	$("input[name='C3']").val("12.3734");
+	$("input[name='C3']").val("18.547");
+	$("input[name='D1']").val("3.9808");
+	$("input[name='D2']").val("6.5476");
+	$("input[name='D3']").val("10.4106");
+	$("input[name='D4']").val("15.924");
+}
+
+function setTo3HoursComp() {
+	$("input[name='A1']").val("5");
+	$("input[name='A2']").val("7.9249");
+	$("input[name='A3']").val("15.5354");
+	$("input[name='A4']").val("21.9415");
+	$("input[name='B1']").val("4.9348");
+	$("input[name='B2']").val("7.3594");
+	$("input[name='B3']").val("12.9864");
+	$("input[name='B4']").val("18.7217");
+	$("input[name='C1']").val("4.5883");
+	$("input[name='C2']").val("6.6476");
+	$("input[name='C3']").val("10.5555");
+	$("input[name='C4']").val("15.2565");
+	$("input[name='D1']").val("3.2848");
+	$("input[name='D2']").val("6.0826");
+	$("input[name='D3']").val("8.7113");
+	$("input[name='D4']").val("12.0016");
+}
+
+function setTo4HoursComp() {
+	$("input[name='A1']").val("4.9594");
+	$("input[name='A2']").val("7.2048");
+	$("input[name='A3']").val("14.225");
+	$("input[name='A4']").val("20.7694");
+	$("input[name='B1']").val("4.7726");
+	$("input[name='B2']").val("6.8036");
+	$("input[name='B3']").val("11.7031");
+	$("input[name='B4']").val("16.715");
+	$("input[name='C1']").val("4.1847");
+	$("input[name='C2']").val("6.2692");
+	$("input[name='C3']").val("9.3929");
+	$("input[name='C4']").val("12.7803");
+	$("input[name='D1']").val("2.6085");
+	$("input[name='D2']").val("5.8217");
+	$("input[name='D3']").val("7.738");
+	$("input[name='D4']").val("9.6912");
+}
+
+function setToInitialYou() {
+	$("input[name='A1']").val("5");
+	$("input[name='A2']").val("10");
+	$("input[name='A3']").val("20");
+	$("input[name='A4']").val("25");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("10");
+	$("input[name='B3']").val("20");
+	$("input[name='B4']").val("25");
+	$("input[name='C1']").val("5");
+	$("input[name='C2']").val("10");
+	$("input[name='C3']").val("20");
+	$("input[name='C4']").val("25");
+	$("input[name='D1']").val("5");
+	$("input[name='D2']").val("10");
+	$("input[name='D3']").val("20");
+	$("input[name='D4']").val("25");
+}
+
+function setTo1HourYou() {
+	$("input[name='A1']").val("5");
+	$("input[name='A2']").val("10");
+	$("input[name='A3']").val("20");
+	$("input[name='A4']").val("25");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("25");
+	$("input[name='B3']").val("20");
+	$("input[name='B4']").val("25");
+	$("input[name='C1']").val("5");
+	$("input[name='C2']").val("10");
+	$("input[name='C3']").val("22.3");
+	$("input[name='C4']").val("22.3");
+	$("input[name='D1']").val("5");
+	$("input[name='D2']").val("10");
+	$("input[name='D3']").val("20");
+	$("input[name='D4']").val("25");
+}
+
+function setTo2HoursYou() {
+	$("input[name='A1']").val("5");
+	$("input[name='A2']").val("10");
+	$("input[name='A3']").val("20");
+	$("input[name='A4']").val("25");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("10");
+	$("input[name='B3']").val("20");
+	$("input[name='B4']").val("25");
+	$("input[name='C1']").val("5");
+	$("input[name='C2']").val("10");
+	$("input[name='C3']").val("5");
+	$("input[name='C3']").val("25");
+	$("input[name='D1']").val("5");
+	$("input[name='D2']").val("10");
+	$("input[name='D3']").val("20");
+	$("input[name='D4']").val("25");
+}
+
+function setTo3HoursYou() {
+	$("input[name='A1']").val("4");
+	$("input[name='A2']").val("10");
+	$("input[name='A3']").val("20");
+	$("input[name='A4']").val("25");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("20");
+	$("input[name='B3']").val("20");
+	$("input[name='B4']").val("25");
+	$("input[name='C1']").val("5");
+	$("input[name='C2']").val("10");
+	$("input[name='C3']").val("15");
+	$("input[name='C4']").val("25");
+	$("input[name='D1']").val("5");
+	$("input[name='D2']").val("4");
+	$("input[name='D3']").val("20");
+	$("input[name='D4']").val("25");
+}
+
+function setTo4HoursYou() {
+	$("input[name='A1']").val("4");
+	$("input[name='A2']").val("10");
+	$("input[name='A3']").val("20");
+	$("input[name='A4']").val("25");
+	$("input[name='B1']").val("5");
+	$("input[name='B2']").val("20");
+	$("input[name='B3']").val("20");
+	$("input[name='B4']").val("25");
+	$("input[name='C1']").val("5");
+	$("input[name='C2']").val("10");
+	$("input[name='C3']").val("15");
+	$("input[name='C4']").val("25");
+	$("input[name='D1']").val("5");
+	$("input[name='D2']").val("4");
+	$("input[name='D3']").val("20");
+	$("input[name='D4']").val("25");
+}
