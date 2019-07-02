57cd13582c0eba43484fddc76d208d65bccb8aef
IvanRossCousins <ivan.cousins@northumbria.ac.uk>
2019-07-02 10:31:00 +0100

    Double pannel update

--- sketch.js
+++ sketch.js
@@ -45,87 +45,161 @@ function tempToColour(temp) {
 }
 
 function setup() {
-	createCanvas(800, 800);
-	background(200);
+	createCanvas(1800, 800);
 	rectMode(CORNER);
 }
 
 function draw() {
 	stroke(0);
 	noFill();
-	for (var x = 0; x < width; x += width/4) {
-		for (var y = 0; y < height; y += height/4) {
+	for (var x = 0; x < 800; x += 200) {
+		for (var y = 0; y < 800; y += 200) {
 			switch((x/50) + (y/200)) {
 			  case 0:
-				var temp = $("input[name='A1']").val();
+				var temp = $(".you[name='A1']").val();
 			    console.log("temp:" + temp);
 				fill(tempToColour(temp));
 			    break;
 			  case 1:
-			    var temp = $("input[name='B1']").val();
+			    var temp = $(".you[name='B1']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 2:
-			    var temp = $("input[name='C1']").val();
+			    var temp = $(".you[name='C1']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 3:
-			    var temp = $("input[name='D1']").val();
+			    var temp = $(".you[name='D1']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 4:
-			    var temp = $("input[name='A2']").val();
+			    var temp = $(".you[name='A2']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 5:
-			    var temp = $("input[name='B2']").val();
+			    var temp = $(".you[name='B2']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 6:
-			    var temp = $("input[name='C2']").val();
+			    var temp = $(".you[name='C2']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 7:
-			    var temp = $("input[name='D2']").val();
+			    var temp = $(".you[name='D2']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 8:
-			    var temp = $("input[name='A3']").val();
+			    var temp = $(".you[name='A3']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 9:
-			    var temp = $("input[name='B3']").val();
+			    var temp = $(".you[name='B3']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 10:
-			    var temp = $("input[name='C3']").val();
+			    var temp = $(".you[name='C3']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 11:
-			    var temp = $("input[name='D3']").val();
+			    var temp = $(".you[name='D3']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 12:
-			    var temp = $("input[name='A4']").val();
+			    var temp = $(".you[name='A4']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 13:
-			    var temp = $("input[name='B4']").val();
+			    var temp = $(".you[name='B4']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 14:
-			    var temp = $("input[name='C4']").val();
+			    var temp = $(".you[name='C4']").val();
 				fill(tempToColour(temp));
 			    break;
 			  case 15:
-			    var temp = $("input[name='D4']").val();
+			    var temp = $(".you[name='D4']").val();
 				fill(tempToColour(temp));
 			    break;
 			  default:
 			    console.log("didn't work");
 			    noFill();
 			}
-			rect(x, y, width/4, height/4);
+			rect(x, y, 200, 200);
+		}
+	}
+	for (var x = 1000; x < 1800; x += 200) {
+		for (var y = 0; y < 800; y += 200) {
+			switch(((x-1000)/50) + (y/200)) {
+			  case 0:
+				var temp = $(".computer[name='A1']").val();
+			    console.log("temp:" + temp);
+				fill(tempToColour(temp));
+			    break;
+			  case 1:
+			    var temp = $(".computer[name='B1']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 2:
+			    var temp = $(".computer[name='C1']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 3:
+			    var temp = $(".computer[name='D1']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 4:
+			    var temp = $(".computer[name='A2']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 5:
+			    var temp = $(".computer[name='B2']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 6:
+			    var temp = $(".computer[name='C2']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 7:
+			    var temp = $(".computer[name='D2']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 8:
+			    var temp = $(".computer[name='A3']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 9:
+			    var temp = $(".computer[name='B3']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 10:
+			    var temp = $(".computer[name='C3']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 11:
+			    var temp = $(".computer[name='D3']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 12:
+			    var temp = $(".computer[name='A4']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 13:
+			    var temp = $(".computer[name='B4']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 14:
+			    var temp = $(".computer[name='C4']").val();
+				fill(tempToColour(temp));
+			    break;
+			  case 15:
+			    var temp = $(".computer[name='D4']").val();
+				fill(tempToColour(temp));
+			    break;
+			  default:
+			    console.log("didn't work");
+			    noFill();
+			}
+			rect(x, y, 200, 200);
 		}
 	}
 }
